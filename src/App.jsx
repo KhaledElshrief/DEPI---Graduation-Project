5e42f957f62ff01dd1052e85adcc129e10c9c937
Ahmed Rushdy <ahmedmohamedrushdy0@gmail.com>
2024-10-03 22:18:48 +0300

    files pushed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import MyHome from "./pages/MyHome";
+import Subscripe from "./pages/Subscripe";
+import WorkAsTeacher from "./pages/WorkAsTeacher";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<MyHome />} />
+            <Route path="/Subscripe" element={<Subscripe />} />
+            <Route path="/WorkAsTeacher" element={<WorkAsTeacher />} />
           </Route>
         </Routes>
       </BrowserRouter>
