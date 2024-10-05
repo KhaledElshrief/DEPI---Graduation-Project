82e154d43ec2d235c0da4c010ac02ccb56608533
Elshami203 <elshamimohamed35@gmail.com>
2024-10-05 18:06:08 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+// import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import MyHome from "./pages/MyHome";
 import Subscribe from "./pages/Subscribe";
 import WorkAsTeacher from "./pages/WorkAsTeacher";
 import BlogCardsContainer from "./components/BlogCardsContainer";
+import TechnologyTools from "./components/TechnologyTools";
+
 const App = () => {
   return (
     <>
-      <BlogCardsContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MyHome />} />
+            <Route path="/Subscribe" element={<Subscribe />} />
+            <Route path="/WorkAsTeacher" element={<WorkAsTeacher />} />
+            <Route  path="/Blogs" element={<BlogCardsContainer />} />
+            <Route path="/TechTools" element={<TechnologyTools />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/components/TechnologyTools.jsx
@@ -0,0 +1,81 @@
+
+const TechnologyTools = () => {
+  return (
+    <>
+         <div className="block isolate box-border pb-0">
+            <section className="custom-widgets-spacing flex mr-auto ml-auto relative">
+            <div class="flex mr-auto ml-auto relative">
+                <div class="relative min-h-[1px] flex">
+                    <div className="relative w-full flex-wrap justify-between p-5">
+                        <div className="w-full relative">
+                            <div className="p-10 bg-white rounded-[10px] border-5 border-solid border-[#273044]">
+                                <h2 className="text-[36px] font-bold p-0 m-0 leading-1 text-[#273044]">"5 أدوات تكنولوجية حديثة ستغيير التعليم الإلكترونى"</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
+            {/* section 2 */}
+            <section className="custom-widgets-spacing flex mr-auto ml-auto relative">
+            <div class="flex mr-auto ml-auto relative">
+                <div class="relative min-h-[1px] flex">
+                    <div className="relative w-full flex-wrap justify-between p-5">
+                        <div className="w-full relative text-center">
+                            <div className="w-full">
+                                <img className="h-auto max-w-full mr-auto ml-auto mt-0 border-none rounded-none shadow-none w-[50%]" src="./images/Elearning.webp" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
+            {/* section 3 */}
+            <section className="custom-widgets-spacing flex mr-auto ml-auto relative">
+                <div className="flex mr-auto ml-auto relative">
+                    <div className="relative min-h-[1px] flex">
+                        <div className="relative w-full flex-wrap justify-between p-5">
+                            <div className="mb-20 mb-20 w-full relative space-y-[20px] space-x-[20px]">
+                                <div className="w-full ml-auto mr-auto">
+                                    <p className="rtl mb-[30px]">فى عصر تسلل فيه التكنولوجيا إلى كل جزء من حياتنا، يمر التعليم بثورة نوعية بفضل سلسلة من الإبتكارات التكنولوجية</p>
+                                    <p className="rtl mb-[30px]">تلك الأدوات لا تسهم فقط فى جعل المواد التعليمية أكثر جاذبية و تفاعلية للطلاب، و لكنها أيضاً تعمل على تحسين تجربة التعليم بشكل شامل
+                                    فى هذا المقال، سنتستعرض 5 من هذة الأدوات التكنولوجية الواعدة</p>
+                                </div>
+                            </div>
+                            {/* الواقع الافتراضي */}
+                            <div className="w-full relative mb-[20px]">
+                            <div className="p-10 bg-white rounded-[10px] border-5 border-solid border-[#273044]">
+                                <h2 className="text-[36px] font-bold p-0 m-0 leading-1 text-[#273044]">1- الواقع المعزز (Augmented Reality - AR) والواقع الافتراضي (Virtual Reality - VR)</h2>
+                            </div>
+                            {/* VR-AR */}
+                            <div className="w-full relative text-center mb-[20px] mt-[20px]">
+                                <div className="w-full">
+                                <img className=" max-w-full mr-auto ml-auto border-none rounded-none shadow-none w-[40%]" src="./images/VR-AR.webp" alt="" />
+                                </div>
+                            </div>
+                            {/* VR-AR content */}
+                            <div className="w-full relative text-center mb-[20px] mt-[20px] ">
+                                <div className="w-full text-[#777]">
+                                    <p className="rtl mb-[30px] block mt-1 ml-0 mr-0 whitespace-normal">تعد تقنيات رائدة فى تحويل طرق تقديم المحتوى التعليمى، حيث تقدم تجارب غامرة تساعد على الفهم و الاحتفاظ بالمعلومات</p>
+                                    <p className="rtl mb-[30px] block mt-1 ml-0 mr-0 whitespace-normal">هى تكنولوجيا تخلق بيانات محاكاة كاملة يمكن للمستخدمين التفاعل معها بشكل غامر باستخدام النظارات أو غرف العرض المجهزة لتقديم تجارب علمية تشبه الواقع و لكن داخل بيئة مسيطر عليها بالكامل بالواقع الأفتراضى VR</p>
+                                    <p className="rtl mb-[30px] block mt-1 ml-0 mr-0 whitespace-normal">و بالنسبة للواقع المعزز فهو يضيف عناصر رقمية إلى العالم الحقيقى، مما يعزز الواقع و يقدم تجارب تعليمية تفاعلية عبر الهاتف و الأجهزة اللوحية</p>
+                                    <p className="rtl mb-[30px] block mt-1 whitespace-normal underline text-bold ml-auto">لها تطبيقات كثيرة:</p>
+                                    <ul className="pl-[20px] mt-0 pb-[10px]">
+                                        <li className="mb-[8px]">يمكن للطالب استخدامها لإستكشاف البيئة البيلوجية المعقدة أو التنقل داخل جسم الإنسان لفهم أفضل<span className="text-bold text-black]">-</span></li>
+                                        <li className="mb-[8px]">إعادة إحياء الحقب التاريخية أو الأماكن الأثرية، مما يوفر للطلاب تجربة زيارة هذا المكان الأثرى و مشاهدة الحدث التاريخى كما لو كان هناك<span className="text-bold text-black]">-</span></li>
+                                        <li className="mb-[8px]">يمكن السفر افتراضياً إلى أى مكان على كوكب الأرض أو حتى خارج الكوكب لاستكشاف الظواهر الطبيعية<span className="text-bold text-black]">-</span></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+         </div>
+    </>
+  )
+}
+
+export default TechnologyTools;
