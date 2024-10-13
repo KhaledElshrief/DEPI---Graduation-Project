5eed88230bb094271411f7a999ccb51627934d33
Ahmed Rushdy <ahmedmohamedrushdy0@gmail.com>
2024-10-14 02:39:58 +0300

    upload

---
+++ src/components/CartItems.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+export default function CartItems({ cartItems, totalPrice, onDelete }) {
+  return (
+    <section id="cartItems">
+      <hr />
+      <div className="row text-center">
+        <p className="col col-1">حذف</p>
+        <p className="col col-5"> المنتج</p>
+        <p className="col col-2">السعر</p>
+        <p className="col col-2">العدد</p>
+        <p className="col col-2">الاجمالي</p>
+      </div>
+      <hr />
+      {cartItems.map((item) => (
+        <Item key={item.id} item={item} onDelete={onDelete} />
+      ))}
+      <div className="row text-center">
+        <hr />
+        <p className="col col-10 "></p>
+        <p className="col col-2 ">{totalPrice} جنيها</p>
+        <hr />
+      </div>
+    </section>
+  );
+}
+
+function Item({ item, onDelete }) {
+  return (
+    <div className="row text-center cartItem">
+      <p className="col col-1 fs-3 delItem" onClick={() => onDelete(item.id)}>
+        &times;
+      </p>
+      <p className="col col-5 itemInfo">
+        <img src={item.img} alt="" />
+        <span>{item.title}</span>
+      </p>
+      <p className="col col-2">{item.price} جنيها </p>
+      <p className="col col-2">1</p>
+      <p className="col col-2">{item.price} جنيها </p>
+    </div>
+  );
+}
---
+++ src/components/YourOrder.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function YourOrder({ cartItems, totalPrice }) {
+  return (
+    <section className="YourOrder text-center">
+      <h2 className="mb-5">طلبك</h2>
+      <hr />
+      <div className="row">
+        <p className="col col-7">الدورة / الاشتراك</p>
+        <p className="col col-5">السعر</p>
+      </div>
+      <hr />
+      {cartItems.map((item) => (
+        <Order key={item.id} item={item} />
+      ))}
+      <hr />
+      <div className="row">
+        <p className="col col-7">اجمال السعر</p>
+        <p className="col col-5">{totalPrice} جنيها</p>
+      </div>
+      <hr />
+    </section>
+  );
+}
+function Order({ item }) {
+  return (
+    <div className="row Order">
+      <p className="col col-7">{item.title}</p>
+      <p className="col col-5">{item.price} جنيها</p>
+    </div>
+  );
+}
